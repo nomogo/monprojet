0caa5e3b1dd27cffe02415ea51a25509d15514ca
nomogo <yahayanomogo@gmail.com>
2025-06-04 00:43:05 +0300

    Delete process.php

--- process.php
+++
@@ -1,7 +0,0 @@
-// Démarrer session et connecter l'utilisateur
-$_SESSION['user_id'] = $utilisateur_id;
-$_SESSION['user_login'] = $login;
-
-// Rediriger vers le site principal (ex : dashboard.php)
-header("Location: dashboard.php");
-exit();
